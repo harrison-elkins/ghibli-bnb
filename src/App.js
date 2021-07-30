5c6d7d92e54faa996edf513e58fdd9d24ba78cb8
Harrison Elkins <harrisonmelkins@gmail.com>
2021-07-29 22:51:34 -0400

    prepared to set up axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/listing/:id" exact component={Listing} />
+        </Switch>
+      </BrowserRouter>
       <Hero />
     </div>
   );
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  //   TODOstate goes here.
+
+  // TODO then axios
+
+  render() {
+    return (
+      <div>
+        <Hero />
+        <LocationCards />
+      </div>
+    );
+  }
+}
+
+export default Home;

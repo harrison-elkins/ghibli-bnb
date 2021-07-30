41b18c92dc695df405ffde5495f422fd96e260c9
Harrison Elkins <harrisonmelkins@gmail.com>
2021-07-30 15:10:27 -0400

    location cards filtered out

--- src/components/LocationCards/LocationCards.jsx
+++ src/components/LocationCards/LocationCards.jsx
@@ -1,22 +1,32 @@
 import card from "../../assets/Mononoke-Irontown.jpeg";
 import "./LocationCards.scss";
 import { Link } from "react-router-dom";
+import image from "../../assets/sosuke-house.jpeg";
 
 const LocationCards = ({ locationData }) => {
   console.log(locationData);
+  const filteredArr = locationData.filter(
+    (location) =>
+      location.name === "Shizuku's Apartment" ||
+      location.name === "Bathhouse" ||
+      location.name === "Fujimoto's Underwater Harbor"
+  );
+  console.log(filteredArr);
   return (
     <main className="cards">
-      {locationData.map((location) => {
+      {filteredArr.map((location) => {
         return (
-          <Link
-            to={`/listing/${location.id}`}
-            className="cards__link"
-            key={locationData.id}
-          >
-            <div class="card__container">
-              <image src={card}></image>
-            </div>
-          </Link>
+          <>
+            <Link
+              to={`/listing/${location.id}`}
+              className="cards__link"
+              key={locationData.id}
+            >
+              <div className="card__container">
+                <image className="card__image" src={image} />
+              </div>
+            </Link>
+          </>
         );
       })}
     </main>

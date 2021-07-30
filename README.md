be4b2bb359acef9a7ba9c21ac52e2f1f29aa8a0e
katieagne <katieagne@gmail.com>
2021-07-29 22:55:04 -0700

    build heroo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 _welcome to ghibli bnb!!_
+=======
+_welcome to ghibli bnb_
+>>>>>>> Stashed changes
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,45 @@
+import image from "../../assets/chihiro-hero.jpeg";
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <main className="hero">
+      <div className="hero__itin">
+        <div className="hero__itin-container">
+          <p className="hero__itin-title">Location</p>
+          <p className="hero__itin-text">Where are you going?</p>
+        </div>
+        <div className="hero__itin-divider"></div>
+        <div className="hero__itin-container">
+          <p className="hero__itin-title">Check in</p>
+          <p className="hero__itin-text">Where are you going?</p>
+        </div>
+        <div className="hero__itin-divider"></div>
+        <div className="hero__itin-container">
+          <p className="hero__itin-title">Check out</p>
+          <p className="hero__itin-text">Add dates</p>
+        </div>
+        <div className="hero__itin-divider"></div>
+        <div className="hero__itin-container">
+          <p className="hero__itin-title">Guests</p>
+          <p className="hero__itin-text">Add Guests</p>
+        </div>
+        <div className="hero__itin-divider"></div>
+        <div className="hero__itin-container">
+          {" "}
+          <div className="hero__itin-search"></div>
+        </div>
+      </div>
+      <div className="hero__title-wrapper">
+        <div className="hero__title-container">
+          <h1 className="hero__title">Magic and Adventure Await You</h1>
+          <div className="hero__cta">
+            <p className="hero__cta-text">Explore Now</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Hero;
